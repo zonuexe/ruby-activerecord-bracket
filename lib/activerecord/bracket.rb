19b0f250b63c6202909f8b5a00fac00cadc00c32
USAMI Kenta <tadsan@zonu.me>
2013-03-20 16:31:14 +0900

    init repo

---
+++ lib/activerecord/bracket.rb
@@ -0,0 +1,7 @@
+require "activerecord/bracket/version"
+
+module ActiveRecord
+  module Bracket
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/bracket/version.rb
@@ -0,0 +1,5 @@
+module ActiveRecord
+  module Bracket
+    VERSION = '0.0.1'
+  end
+end

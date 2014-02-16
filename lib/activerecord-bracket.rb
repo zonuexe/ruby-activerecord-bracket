378fe97614e7aaecf05a83eca0f6d587d4cea9ec
USAMI Kenta <tadsan@zonu.me>
2014-02-16 14:26:41 +0900

    Remove redundant definitions

--- lib/activerecord-bracket.rb
+++ lib/activerecord-bracket.rb
@@ -1,10 +1,2 @@
 require 'active_support'
-require 'activerecord-bracket/version'
-
-# a Namespece of ActiveRecord
-module ActiveRecord
-  # a Namespace of ActiveRecord::Bracket
-  module Bracket;  end
-end
-
 require 'activerecord-bracket/method'
